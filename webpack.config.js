fc5a5f5936bc4cfbd00d943dfe085e8686e875b6
s-light <github@s-light.eu>
2018-07-12 18:33:13 +0200

    added font loading option

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,61 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const devMode = process.env.NODE_ENV !== 'production';
 
 
+const my_plugins = [
+    new HtmlWebpackPlugin({
+        template: './src/timeline_app.html',
+        // embed all javascript and css inline
+        // inlineSource: '.(js|css)$',
+        alwaysWriteToDisk: true,
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    new HtmlWebpackInlineSourcePlugin(),
+    new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+    }),
+];
+
+const my_rules = [
+    // {
+    //     test: /\.css$/,
+    //     use: [
+    //         {
+    //             loader: 'style-loader',
+    //             options: {
+    //                 sourceMap: true
+    //             }
+    //         },
+    //         'css-loader',
+    //         // { loader: 'style-loader/url', },
+    //         // { loader: 'file-loader' },
+    //     ]
+    // },
+    {
+        test: /\.scss$/,
+        use: [{
+            // fallback to style-loader in development
+            // devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            // loader: "style-loader"
+            loader: MiniCssExtractPlugin.loader
+        }, {
+            loader: 'css-loader', options: {
+                sourceMap: true
+            }
+        }, {
+            loader: 'sass-loader', options: {
+                sourceMap: true
+            }
+        }]
+    },
+    {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader'],
+    }
+];
+
 module.exports = {
     entry: './src/index.js',
     devtool: 'source-map',
@@ -23,57 +78,8 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/timeline_app.html',
-            // embed all javascript and css inline
-            // inlineSource: '.(js|css)$',
-            alwaysWriteToDisk: true,
-        }),
-        new HtmlWebpackHarddiskPlugin(),
-        new HtmlWebpackInlineSourcePlugin(),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-    ],
-    module: {
-        rules: [
-            // {
-            //     test: /\.css$/,
-            //     use: [
-            //         {
-            //             loader: 'style-loader',
-            //             options: {
-            //                 sourceMap: true
-            //             }
-            //         },
-            //         'css-loader',
-            //         // { loader: 'style-loader/url', },
-            //         // { loader: 'file-loader' },
-            //     ]
-            // },
-            {
-                test: /\.scss$/,
-                use: [{
-                    // fallback to style-loader in development
-                    // devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    // loader: "style-loader"
-                    loader: MiniCssExtractPlugin.loader
-                }, {
-                    loader: 'css-loader', options: {
-                        sourceMap: true
-                    }
-                }, {
-                    loader: 'sass-loader', options: {
-                        sourceMap: true
-                    }
-                }]
-            }
-        ]
-    },
+    plugins: my_plugins,
+    module: {rules: my_rules},
     mode: 'development'
     // mode: 'production'
 };
