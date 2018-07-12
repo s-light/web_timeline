c9b2de2a0de602fa7e83e070aa07b4e6295862f0
s-light <github@s-light.eu>
2018-07-12 14:38:05 +0200

    html generation seams to work :-)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 import moment from 'moment';
-
+// import './style.css';
 
 
 window.addEventListener('load', function(event) {
@@ -11,8 +11,15 @@ function showNow() {
     console.group('show Now');
     let result = '';
     let el = document.querySelector('#now');
-    if (el) {
-        el.textContent = moment().format('DD.MM.YYYY H:mm:ss');
+    console.log('el', el);
+    if (!el) {
+        console.log('create new section element');
+        el = document.createElement('section');
+        el.id = 'now';
+        console.log('el', el);
+        const el_body = document.querySelector('body');
+        el_body.prepend(el);
     }
+    el.textContent = moment().format('DD.MM.YYYY H:mm:ss');
     console.groupEnd();
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSourcePlugin = require(
-    'html-webpack-inline-source-plugin');
-
+    'html-webpack-inline-source-plugin'
+);
 // const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
 // https://github.com/thegc/html-webpack-inline-svg-plugin
 
@@ -19,11 +19,37 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Timeline App',
+            // template: 'index.template.ejs',
+            template: './src/timeline_app.html',
+            // title: 'Timeline App',
             // embed all javascript and css inline
             // inlineSource: '.(js|css)$',
         }),
         new HtmlWebpackInlineSourcePlugin()
     ],
+    module: {
+        rules: [
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         {
+            //             loader: 'style-loader',
+            //             options: {
+            //                 sourceMap: true
+            //             }
+            //         },
+            //         'css-loader',
+            //         // { loader: 'style-loader/url', },
+            //         // { loader: 'file-loader' },
+            //     ]
+            // },
+            // {
+            //     test: /\.html$/,
+            //     use: [
+            //         'file-loader?name=[path][name].[ext]!extract-loader!html-loader'
+            //     ]
+            // },
+        ]
+    },
     mode: 'development'
 };
